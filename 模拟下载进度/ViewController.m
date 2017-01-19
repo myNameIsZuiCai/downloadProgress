b85fce21fa5f0f3f2dc74faf63e1025057688507
“shanghaijunheng” <78899500@qq.com>
2017-01-19 16:41:52 +0800

    Initial Commit

---
+++ 模拟下载进度/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  模拟下载进度
+//
+//  Created by 上海均衡 on 2017/1/19.
+//  Copyright © 2017年 上海均衡. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ 模拟下载进度/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  模拟下载进度
+//
+//  Created by 上海均衡 on 2017/1/19.
+//  Copyright © 2017年 上海均衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
